75895a4fb69af58798c73ceede1edbbf5b8dcf37
Jason Hagglund <hagglundized@gmail.com>
2024-12-03 12:33:09 -0600

    Nevermind, not doing anything existing specs don't already do

--- test-project/lib/handlers.rb
+++
@@ -1,4 +0,0 @@
-require "rita"
-
-require_relative "lita/handlers/ping"
-require_relative "lita/handlers/redis_test"
--- test-project/lita_config.rb
+++
@@ -1,9 +0,0 @@
-require_relative "lib/handlers"
-
-Lita.configure do |config|
-  config.robot.name = "testbot"
-
-  config.robot.adapter = :shell
-  config.robot.log_level = :debug
-  config.http.port = 3333
-end
